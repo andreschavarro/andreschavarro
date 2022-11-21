99cd5ece4a7e352e2b798d8a0407a2e3fe72caf7
Andres felipe <aanaconachavarro@gmail.com>
2022-11-21 13:43:25 -0500

    cammit para agregar a malopez

--- app.js
+++ app.js
@@ -11,11 +11,30 @@ document.getElementById('formulario').addEventListener('submit', function(e) {
     .then(res => res.json())
     .then(data => {
         if(data == 'true') {
-            document.getElementById('txt_usuario').value = '';
-            document.getElementById('txt_nombre').value = '';
-            document.getElementById('int_edad').value = '';
-            document.getElementById('txt_vivienda').value = '';
-            document.getElementById('txt_condicion').value = '';
+            document.getElementById('nombre ').value = '';
+            document.getElementById('correo ').value = '';
+            document.getElementById('telefono ').value = '';
+            document.getElementById('jornada ').value = '';
+            document.getElementById('estadoactual ').value = '';
+            document.getElementById('estadocivil ').value = '';
+            document.getElementById('sexo ').value = '';
+            document.getElementById('Genero ').value = '';
+            document.getElementById('tranporte ').value = '';
+            document.getElementById('barrio ').value = '';
+            document.getElementById('tiempo_del_trayecto').value = '';
+            document.getElementById('hora_de_dormir ').value = '';
+            document.getElementById('hora_de_despertarse ').value = '';
+            document.getElementById('sueño ').value = '';
+            document.getElementById('estudio_adicional ').value = '';
+            document.getElementById('ingles ').value = '';
+            document.getElementById('asignatura ').value = '';
+            document.getElementById('especializacion ').value = '';
+            document.getElementById('emprendimiento ').value = '';
+            document.getElementById('catedra ').value = '';
+            document.getElementById('cursos ').value = '';
+            document.getElementById('relaciones').value = '';
+            document.getElementById('amigos ').value = '';
+            document.getElementById('profesores ').value = '';
             alert('El usuario se insertó correctamente.');
         } else {
             console.log(data);
--- registrar.php
+++ registrar.php
@@ -1,23 +1,64 @@
 <?php
 
-$usuario = isset($_POST['txt_usuario']) ? $_POST['txt_usuario'] : '';
-$nombre = isset($_POST['txt_nombre']) ? $_POST['txt_nombre'] : '';
-$edad = isset($_POST['int_edad']) ? $_POST['int_edad'] : '';
-$vivienda = isset($_POST['txt_vivienda']) ? $_POST['txt_vivienda'] : '';
-$condicion = isset($_POST['txt_condicion']) ? $_POST['txt_condicion'] : '';
+$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+$correo = isset($_POST['correo']) ? $_POST['correo'] : '';
+$telefono = isset($_POST['telefono']) ? $_POST['telefono'] : '';
+$jornada = isset($_POST['jornada']) ? $_POST['jornada'] : '';
+$estadoactual = isset($_POST['estadoactual']) ? $_POST['estadoactual'] : '';
+$estadocivil = isset($_POST['estadocivil']) ? $_POST['estadocivil'] : '';
+$sexo = isset($_POST['sexo']) ? $_POST['sexo'] : '';
+$genero = isset($_POST['genero']) ? $_POST['genero'] : '';
+$tranporte = isset($_POST['tranporte']) ? $_POST['tranporte'] : '';
+$barrio = isset($_POST['barrio']) ? $_POST['barrio'] : '';
+$tiempo_del_trayecto = isset($_POST['tiempo_del_trayecto']) ? $_POST['tiempo_del_trayecto'] : '';
+$hora_de_dormir  = isset($_POST['hora_de_dormir ']) ? $_POST['hora_de_dormir '] : '';
+$hora_de_despertarse  = isset($_POST['hora_de_despertarse ']) ? $_POST['hora_de_despertarse '] : '';
+$sueño  = isset($_POST['sueño ']) ? $_POST['sueño '] : '';
+$estudio_adicional = isset($_POST['estudio_adicional']) ? $_POST['estudio_adicional'] : '';
+$ingles = isset($_POST['ingles']) ? $_POST['ingles'] : '';
+$asignatura  = isset($_POST['asignatura ']) ? $_POST['asignatura '] : '';
+$especializacion = isset($_POST['especializacion']) ? $_POST['especializacion'] : '';
+$emprendimiento = isset($_POST['emprendimiento']) ? $_POST['emprendimiento'] : '';
+$catedra = isset($_POST['catedra']) ? $_POST['catedra'] : '';
+$cursos = isset($_POST['cursos']) ? $_POST['cursos'] : '';
+$relaciones  = isset($_POST['relaciones ']) ? $_POST['relaciones '] : '';
+$amigos  = isset($_POST['amigos']) ? $_POST['amigos'] : '';
+$profesores  = isset($_POST['profesores']) ? $_POST['profesores'] : '';
+
 
 try {
 
-    $conexion = new PDO("mysql:host=localhost;port=3306;dbname=prueba_db", "root", "");
+    $conexion = new PDO("mysql:host=localhost;port=3306;dbname=data", "root", "");
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
-    $pdo = $conexion->prepare('INSERT INTO usuarios(usuario, nombre, edad, vivienda, condicion) VALUES(?, ?, ?, ?, ?)');
-    $pdo->bindParam(1, $usuario);
-    $pdo->bindParam(2, $nombre);
-    $pdo->bindParam(3, $edad);
-    $pdo->bindParam(4, $vivienda);
-    $pdo->bindParam(5, $condicion);
+    $pdo = $conexion->prepare('INSERT INTO registro(nombre, correo, telefono, jornada , estadoactual, estadocivil, sexo, Genero, tranporte, barrio, tiempo_del_trayecto, hora_de_dormir, hora_de_despertarse, sueño,estudio_adicional, ingles, asignatura, especializacion, emprendimiento, catedr, cursos, relaciones, amigos, profesores) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+    
+    $pdo->bindParam(1, $nombre);
+    $pdo->bindParam(2, $correo);
+    $pdo->bindParam(3, $telefono);
+    $pdo->bindParam(4, $jornada);
+    $pdo->bindParam(5, $estadoactual);
+    $pdo->bindParam(6, $estadocivil);
+    $pdo->bindParam(7, $sexo);
+    $pdo->bindParam(8, $genero);
+    $pdo->bindParam(9, $tranporte);
+    $pdo->bindParam(10, $barrio);
+    $pdo->bindParam(11, $tiempo_del_trayecto);
+    $pdo->bindParam(12, $hora_de_dormir);
+    $pdo->bindParam(13, $hora_de_despertarse);
+    $pdo->bindParam(14, $sueño);
+    $pdo->bindParam(15, $estudio_adicional);
+    $pdo->bindParam(16, $ingles);
+    $pdo->bindParam(17, $asignatura);
+    $pdo->bindParam(18, $especializacion);
+    $pdo->bindParam(19, $emprendimiento);
+    $pdo->bindParam(20, $catedra);
+    $pdo->bindParam(21, $cursos);
+    $pdo->bindParam(22, $relaciones);
+    $pdo->bindParam(23, $amigos);
+    $pdo->bindParam(24, $profesores);
+    
     $pdo->execute() or die(print($pdo->errorInfo()));
 
     echo json_encode('true');
